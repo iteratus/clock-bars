356e1adce2cf1801f251f7b0110fe288a95d25ab
Michael Huber <hmc@xxxlgroup.com>
2021-02-26 15:39:17 +0100

    initial setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const elements = document.querySelectorAll(".clock > *");
+
+console.log(elements);
+
+setInterval(() => {
+    const date = new Date();
+
+    const digets = [
+        ...("0" + date.getHours()).slice(-2).split(""),
+        ...("0" + date.getMinutes()).slice(-2).split(""),
+        ...("0" + date.getSeconds()).slice(-2).split("")
+    ];
+
+    elements.forEach((element, index) => {
+        element.className = `pos-${digets[index]}`;
+    });
+}, 1000);
