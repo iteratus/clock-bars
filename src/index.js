8e9445ab7d44156255e8cf63c7e4139fa64c337b
Michael Huber <hmc@xxxlgroup.com>
2021-03-02 18:18:08 +0100

    Simplify code

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ const elements = document.querySelectorAll(".clock > *");
 const clock = () => {
   const date = new Date();
 
-  const digits = [
-    ...`0${date.getHours()}`.slice(-2).split(""),
-    ...`0${date.getMinutes()}`.slice(-2).split(""),
-    ...`0${date.getSeconds()}`.slice(-2).split(""),
-  ];
+  const digits =
+    `0${date.getHours()}`.slice(-2) +
+    `0${date.getMinutes()}`.slice(-2) +
+    `0${date.getSeconds()}`.slice(-2);
 
   elements.forEach((element, index) => {
     // eslint-disable-next-line no-param-reassign
