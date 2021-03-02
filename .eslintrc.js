523be0984fe8bea47b28be14abedf09ddf201b07
Michael Huber <hmc@xxxlgroup.com>
2021-03-02 10:22:15 +0100

    Fix linting problems

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+  ],
+  env: {
+    browser: true,
+    es6: true,
+  },
+  plugins: [
+    'import',
+  ],
+  rules: {
+    'no-unused-expressions': 'off',
+    'import/no-dynamic-require': 'off',
+    'global-require': 'off',
+  },
+};
